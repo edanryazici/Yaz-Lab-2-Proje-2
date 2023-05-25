2399e6373429d6136ec40a36e7dc2709517c228d
edanryazici <75092705+edanryazici@users.noreply.github.com>
2023-05-25 18:23:19 +0300

    update main.py
    algorithms.py, main.py import edildi ve karşılaştırma sayısı gösterimi eklendi

--- YazLab2-Proje2/main.py
+++ YazLab2-Proje2/main.py
@@ -1,13 +1,10 @@
 import wx
 import wx.lib.agw.flatnotebook as fnb
 from customs import CustomSlider, CustomDropdown, CustomButton
-
+from algorithms import selection_sort, merge_sort, quick_sort, bubble_sortt, insertion_sort
 
 
 class MainFrame(wx.Frame):
-
-
-
 
     def __init__(self, parent):
         wx.Frame.__init__(self, parent, title="Sıralama Projesi", size=(800, 600))
@@ -31,15 +28,25 @@ class MainFrame(wx.Frame):
         self.algorithm_radios = wx.RadioBox(self.left_panel, choices=self.algorithms, style=wx.RA_VERTICAL)
 
         # Sıralama türü açılır menüsü
-        self.sort_type_label = wx.StaticText(self.left_panel, label="Sıralama Türü:")
+        self.sort_type_label = wx.StaticText(self.left_panel, label="Grafik Türü:")
         self.sort_type_choices = ["Dağılım Grafiği", "Sütun Grafiği", "Kök Grafiği"]
         self.sort_type_dropdown = CustomDropdown(self.left_panel, choices=self.sort_type_choices)
 
         # Butonlar
         self.create_button = CustomButton(self.left_panel, label="Oluştur", background_color=(46, 204, 113))
         self.start_button = CustomButton(self.left_panel, label="Başlat", background_color=(52, 152, 219))
-        self.pause_button = CustomButton(self.left_panel, label="Durdur", background_color=(255, 170, 0))
+        self.pause_button = CustomButton(self.left_panel, label="Durdur/ Devam Et", background_color=(255, 170, 0))
         self.reset_button = CustomButton(self.left_panel, label="Sıfırla", background_color=(239, 35, 60))
+
+        # Karşılaştırma sayısı ve analiz sonucunu tutacak değişkenler
+        self.comparison_count = 0
+        self.analysis_result = ""
+
+        # Karşılaştırma sayısını gösteren etiket
+        self.comparison_label = wx.StaticText(self.left_panel, label="Karşılaştırma Sayısı: 0")
+
+        # Analiz sonucunu gösteren etiket
+        self.analysis_label = wx.StaticText(self.left_panel, label="")
 
         # Sol Panel Sıralama
         self.left_sizer = wx.BoxSizer(wx.VERTICAL)
@@ -54,6 +61,8 @@ class MainFrame(wx.Frame):
         self.left_sizer.Add(self.start_button, 0, wx.EXPAND | wx.ALL, 10)
         self.left_sizer.Add(self.pause_button, 0, wx.EXPAND | wx.ALL, 10)
         self.left_sizer.Add(self.reset_button, 0, wx.EXPAND | wx.ALL, 10)
+        self.left_sizer.Add(self.comparison_label, 0, wx.ALIGN_CENTER_HORIZONTAL)
+        self.left_sizer.Add(self.analysis_label, 0, wx.ALIGN_CENTER_HORIZONTAL)
 
         self.left_panel.SetSizer(self.left_sizer)
 
@@ -86,7 +95,20 @@ class MainFrame(wx.Frame):
         value = self.speed_slider.GetValue()
         self.speed_value_label.SetLabel("Hız: " + str(value))
 
- 
+    def update_comparison_count(self):
+        self.comparison_label.SetLabel("Karşılaştırma Sayısı: " + str(self.comparison_count))
+
+    def update_analysis_result(self):
+        self.analysis_label.SetLabel(self.analysis_result)
+
+    def show_analysis_result(self):
+        # Sıralama işlemi tamamlandığında karşılaştırma sayısı ve analiz sonucunu ekrana yazdır
+        self.update_comparison_count()
+        self.update_analysis_result()
+
+        # Analiz sonuçlarını güncelledikten sonra paneli yenile
+        self.panel.Layout()
+
 
 if __name__ == '__main__':
     app = wx.App()
